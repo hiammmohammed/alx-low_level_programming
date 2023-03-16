942a4ac774522d703631419e2677bdae5af95675
hiammohammed <haiammohammed@gmail.com>
2023-03-16 09:08:48 -0700

    helloworld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,9 @@
  *
  *  Return : Always 0 (Sucess)
  */
- int main(void)
+int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-
-             return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
