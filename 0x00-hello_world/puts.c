110877f42936ce9bfe5aa4dcdeb4d4f105470090
hiammohammed <haiammohammed@gmail.com>
2023-03-16 10:38:34 -0700

    hello world

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+
+* main - A program that print a line wi
+
+th puts function
+
+*
+
+* Return: 0 (Success)
+
+
+
+*/
+
+int main(void)
+{
+puts("\"Programming is like bu ilding a multilingual puzzle"
+);
+return (0);
+}
