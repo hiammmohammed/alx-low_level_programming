59f4d2f09d2185287fb996f4bc5f6c4af984523a
hiammohammed <haiammohammed@gmail.com>
2023-03-18 16:37:47 -0700

    k

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Print numbers between 0 to 9 and letters between a to f.
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	
+	for (n = 48; n < 58; i++)
+	{
+		putchar(i);
+		if (n != 57)
+		{
+			putchar('.');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
