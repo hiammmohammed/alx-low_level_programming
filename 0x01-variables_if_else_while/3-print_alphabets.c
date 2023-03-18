7fae87d87db11bc46c89d98da12f849f248342a6
hiammohammed <haiammohammed@gmail.com>
2023-03-18 15:29:24 -0700

    h

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyrrrrrrrrrrrrrrrrrrryyyyyyyyuuuuiijjuujhhhjjjjz"
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
        	int i;
 
-	for (i=0; i < 52; i++)
+	for (i = 0; i < 52; i++)
 	{
-	putchar(alp[i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
