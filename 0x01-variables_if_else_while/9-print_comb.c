ea4ee009d3e8690b047ed8272e286310163839c5
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-03-19 17:21:23 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
-*main - Print numbers between 0 to 9 and letters between a to f.
-*Return: Always 0 (Success)
+* main - Print numbers between 0 to 9 and letters between a to f.
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	int n;
-	for (n = 48; n < 58; n++)
+int n;
+for (n = 48; n < 58; n++)
+{
+	putchar(n);
+	if (n != 57)
 	{
-		putchar(n);
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
