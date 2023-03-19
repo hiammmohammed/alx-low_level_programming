e8388303c4fc15bfa7e6a6975ed46c9e33bac431
hiammohammed <haiammohammed@gmail.com>
2023-03-19 07:41:27 -0700

    x

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,17 @@
 /**
  *main Prints the alphabet.
  *
- * Return: Always B (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-       	int i;
-	for (i=0; i < 26; i++)
-	{
-		putchar(alp[i]);
-	}
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+	putchar(alp[i]);
+}
 
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
