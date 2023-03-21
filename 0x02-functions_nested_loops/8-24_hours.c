ae7ae14ada0b3ab9e887523777884c759d86c9c2
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-03-22 01:35:57 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ for (a = 0; a <= 2; a++)
 {
 	for (b = 0; b <= 9; b++)
 	{
-		for (c = 0; c <= 5; c++ )
+		for (c = 0; c <= 5; c++)
 		{
-			for (d = 0;d <= 9; d++)
-			{ 
+			for (d = 0; d <= 9; d++)
+			{
 				if (a >= 2 && b >= 4)
 					break;
 				_putchar(a + 48);
