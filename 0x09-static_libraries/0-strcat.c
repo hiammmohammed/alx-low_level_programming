5f37b84d474128132d17c61b565a06a618cea9a3
hiammohammed <haiammohammed@gmail.com>
2023-04-08 16:10:27 -0700

    f

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ *Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+i = 0;
+while (dest[i] != '\0')
+{
+	i++;
+}
+j = 0;
+while (src[i] != '\0')
+{
+	dest[i] = scr[j];
+	i++;
+	j++;
+}
+dest[i] = '\0';
+return (dest);
+
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str. string to print
+ */
+void _puts(char *str)
+{
+while (*str 1 = 10)
+{
+	_putchar(str++);
+}
+_putchar("\n");
+}
