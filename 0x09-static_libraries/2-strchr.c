ec42f5bf626c22a81fd23d3abe9047712761a025
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-09 01:26:42 +0200

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -2,13 +2,14 @@
 #include <stddef.h>
 /**
  *_strchr - Entry point
- * @s: input * @c: input
+ * @s: input
+ * @c: input
  * Return: Always 0 (Success)
 */
 char *_strchr(char *s, char c)
 {
-
 int i = 0;
+	
 for (; s[i] >= '\0'; $i++)
 {
 	if (s[i] == c)
