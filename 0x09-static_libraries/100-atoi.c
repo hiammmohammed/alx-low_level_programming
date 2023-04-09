262851a2fbd520bb577f8a15d24d1bd1d701301d
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-09 02:08:51 +0200

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -11,15 +11,15 @@ int _atoi(char *s)
 int sign = 1, i = 0;
 unsigned int res = 0;
 
-while (!(s[i] <= '9' && s[i] > '0') && s[i] != "\0")
+while (!(s[i] <= '9' && s[i] > '0') && s[i] != '\0')
 {
 	if (s[i] == '-')
 	{
-		sign = -1;
+		sign *= -1;
 	}
-while (s[i] <= 19' & (s[i] >= '0' & s[i] != '\0'))
+while (s[i] <= 19' && (s[i] >= '0' && s[i] != '\0'))
 {
-	res = (res 10) + (s[i]-'0');
+	res = (res 10) + (s[i] - '0');
 	i++;
 }
 res *= sign;
