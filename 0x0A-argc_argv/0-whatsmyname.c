809f3c8a628f182884585fd3caa8dd4332ef6c7a
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-09 16:31:48 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include "main h"
+
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Entry point
+ * @argc: count of arguments
+ * @argv: array of string arguments
  *
- *Description: print alphabets in lowercase using the putchar
+ * Description: print alphabets in lowercase using the putchar
  *
- * Return Always 0 (Success)
+ * Return: returns 0 (Success)
  */
-int main(_attribute_((unused)) int argc, char *argv[])
+
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-  printf("%s\n", *argv);
-  return (0);
-}
+	printf("%s\n", argv[0]);
+
+	return (0);
+ }
