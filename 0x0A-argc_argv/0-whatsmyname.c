3ebd10ecd163a53f30f160523ed8a9735c0acfc3
hiammohammed <haiammohammed@gmail.com>
2023-04-09 07:14:58 -0700

    r

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,8 @@
 #include "main h"
 /**
  * main - prints the name of the program
- * @arge number of arguments
- * @argy array of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return Always 0 (Success)
  */
--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * _putchar - writes the character c to stdout
- * @c Ther character to print
+ * @c: Ther character to print
  *
  * Retum: On success 1.
  * On error -1 is returned and errno is set appropriately.
