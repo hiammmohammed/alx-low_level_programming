e30e1c98c803abafe28e0d9895ff2f58e0e942e3
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-09 16:50:34 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "main h"
 /**
- * main - prints the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Entry point
+ * @argc: count of arguments
+ * @argv: array of string arguments
  *
  * Description: print alphabets in lowercase using the putchar
  *
- * Return Always 0 (Success)
+ * Return: returns 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 (void) argv; /*ignore argv[]*/
