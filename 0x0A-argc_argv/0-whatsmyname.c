cc512ccd69484083908793a8369431376cd41f6c
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-09 16:28:07 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,9 @@
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  *Description: print alphabets in lowercase using the putchar
- * 
+ *
  * Return Always 0 (Success)
  */
 int main(_attribute_((unused)) int argc, char *argv[])
