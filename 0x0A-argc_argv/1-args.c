6780fc2ab1588fcb2d45aafc63e0994fc6be60fb
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-09 16:46:25 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,12 +5,14 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
+ * Description: print alphabets in lowercase using the putchar
+ *
  * Return Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 (void) argv: /*ignore argv[]*/
-printf("%d\n" ,argc - 1);
+printf("%d\n", argc - 1);
 
-return (0)
+return (0);
 }
