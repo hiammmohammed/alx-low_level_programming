41e2969b159c9a24a2096fa916ce03ca15922641
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-09 02:52:44 +0200

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,13 @@
 #include "main.h"
+#include<stdio.h>
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
 void _puts(char *str)
+  
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual builds");
 return (0);
 }
