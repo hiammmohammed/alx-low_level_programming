b3bcd2c816db82dc093384a229db125f33710776
hiammohammed <haiammohammed@gmail.com>
2023-04-17 16:37:47 -0700

    k

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,11 @@
-#include<stdlib.h>
-#include"dog.h"
+#ifndef DOG_H
+#define DOG_H
 
-/**
- * init_dog - initializes a variables of tybe struct dog
- * @d: pointer to struct dog initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-if (d == NULL)
-{
-	d = malloc(sizeof(struct dog));
-}
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
