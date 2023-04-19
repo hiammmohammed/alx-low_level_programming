24233a678ae9d3704ffbba7d4dd5a942bc7b4951
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-20 00:41:13 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,8 +1,13 @@
+#include "3-calc.h"
+
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
+
 /**
  * op_add - Returns the sum of two numbers.
  * @a: The first number.
@@ -14,17 +19,18 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
- /**
-  * op_sub - Return the difference of two numbers.
-  * @a: The frist number.
-  * @b: The second number.
-  *
-  * Return: The difference of a and b.
-  */
+/**
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
  * op_mul - Returns the product of two numbers.
  * @a: The first number.
@@ -47,6 +53,7 @@ int op_div(int a, int b)
 {
 	return (a / b);
 }
+
 /**
  * op_mod - Returns the remainder of the division of two numbers.
  * @a: The first number.
