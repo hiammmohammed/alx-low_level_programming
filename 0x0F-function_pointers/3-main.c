14b360a0ce880cf9b1c1b6af3c89c653f636bcef
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-20 00:10:24 +0200

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  *
  * Return: Always 0.
  */
-int main(int__attribute__((__unused__))argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 int num1, num2;
 char *op;
@@ -31,6 +31,6 @@ if (get_op_func(op) == NULL || op[1] != '\0')
 if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 
 	printf("%d\n", get_op_func(op)(num1, num2));
-	return (0)
+	return (0);
 }
 
