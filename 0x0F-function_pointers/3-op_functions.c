4ea9a0b752c1c17a1824713131b7776c999877b8
hiammmohammed <117843259+hiammmohammed@users.noreply.github.com>
2023-04-20 00:36:08 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -5,55 +5,56 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
  /**
   * op_add - Returns the sum of two numbers.
-  * @a: The frist number
-  * @b: The second number
+  * @a: The frist number.
+  * @b: The second number.
   *
-  *Return: The sum of a and b
+  *Return: The sum of a and b.
   */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
  /**
-  * op_sub - Return the difference of two numbers
-  * @a: The frist number
-  * @b: The second number
+  * op_sub - Return the difference of two numbers.
+  * @a: The frist number.
+  * @b: The second number.
   *
-  * Return: The difference of a and b
+  * Return: The difference of a and b.
   */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mul Rturns the product of two numbers
- * @a: The frist number
- * @b: The second number
+ * op_mul Rturns the product of two numbers.
+ * @a: The frist number.
+ * @b: The second number.
  *
- * Return: The product of a and b
+ * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
- * op_div Rturns the division of two numbers
- * @a: The frist number
- * @b: The second number
+ * op_div Rturns the division of two numbers.
+ * @a: The frist number.
+ * @b: The second number.
  *
- * Return: The quotient of a and b
+ * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
- * op_mod Rturns the remainder of the division of two numbers
- * @a: The frist number
- * @b: The second number
+ * op_mod Rturns the remainder of the division of two numbers.
+ * @a: The frist number.
+ * @b: The second number.
  *
- * Return: The remainder of the division of a by b
+ * Return: The remainder of the division of a by b.
  */
+int op_mod(int a, int b)
 {
 	return ( a % b);
 }
