f6fc3557c9757ed6f544bd8b8502223ddb26cad3
hiammohammed <haiammohammoud@gmail.com>
2023-11-17 04:28:10 -0800

    j

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void first(void) __attribute__ ((constructor));
  * function is executed
  */
 void first(void)
-{	
+{
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
