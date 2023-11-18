14d8c95fb9d83247756ae7d6cbcdd0c6ecad4f3f
hiammohammed <haiammohammoud@gmail.com>
2023-11-18 09:41:30 -0800

    k

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -15,6 +15,8 @@ void _puts_recursion(char *s)
 	_puts_recursion(s + 1);
 	}
 	else
+	{
 	_putchar('\n');
+	}
 }
 
--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -4,7 +4,7 @@
 * _putchar - writes the character c to stdout
 * @c: The character to print
 *
-* Return: On success 
+* Return: On success
 * On error, -1 is returned, and errno is set appropriately.
 */
 int _putchar(char c)
